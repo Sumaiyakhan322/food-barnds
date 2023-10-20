e3e47a9edb7ad5d8539bf1d758023b54558c4c66
Sumaiyakhan322 <sumaiya.khan32665@gmail.com>
2023-10-20 14:52:30 +0600

    added 2 extra section to home page

--- src/Components/Navber.jsx
+++ src/Components/Navber.jsx
@@ -46,11 +46,11 @@ isPending ? "pending" : isActive ? "text-red-800" : ""
              <div className="">
    <div className="navbar p-4 sticky inset-0 z-10  flex flex-col gap-5 md:flex-row justify-between">
         <div className="navbar-start">
-          <div className="dropdown">
+          <div className="dropdown ">
             <label tabIndex={0} className="btn btn-ghost lg:hidden">
               <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
             </label>
-            <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow  rounded-box w-52 font-bold text-lg space-x-2 ">
+            <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow  rounded-box w-52 font-bold text-lg space-x-2 bg-white dark-bg ">
            {navLinks}
             </ul>
           </div>
